0464cdd3b5a7eefd42ae7a0a3fe5151bfa056060
Niko Felger <niko.felger@gmail.com>
2020-10-16 15:18:41 +0200

    API docs: response fields and general notes

--- rip_api/api_schemas.py
+++ rip_api/api_schemas.py
@@ -1,17 +1,17 @@
 from typing import List, Optional, Union
 
 import humps
-from pydantic import BaseModel, create_model, validator
+from pydantic import BaseModel, Field, create_model, validator
 
 from . import PUBLIC_ASSET_ROOT, urls
 
 
 class ContentItemBasicFields(BaseModel):
     type: str
-    id: str
-    url: str
-    name: str
-    title: Optional[str]
+    id: str = Field(..., description="Unique ID")
+    url: str = Field(..., description="Resource link")
+    name: str = Field(..., description="Item name, e.g. paragraph number or section")
+    title: str = Field(None, description="Section or article title")
 
     @classmethod
     def _attrs_dict_from_item(cls, item):
@@ -38,12 +38,13 @@ class ContentItemBasicFields(BaseModel):
         return model_type(**attrs)
 
 
+class ContentItemReference(BaseModel):
+    type: str = Field(..., description="Type of containing heading (\"heading\" or \"headingArticle\")")
+    id: str = Field(..., description="ID of containing heading")
+
+
 class ContentItemAllFields(ContentItemBasicFields):
-    parent: Optional[create_model(
-        'ContentItemReference',  # noqa
-        type=(str, ...),
-        id=(str, ...)
-    )]
+    parent: ContentItemReference = Field(None, description="Containing section heading (see docs for /laws/{slug})")
 
     @validator("type", allow_reuse=True, check_fields=False)
     def type_string_must_match_model_type(cls, v):
@@ -76,27 +77,41 @@ class ContentItemAllFields(ContentItemBasicFields):
         return attrs
 
 
-class ContentItemWithBodyAndFootnotes(ContentItemAllFields):
-    body: Optional[str] = ...
-    footnotes: Optional[str] = ...
-    documentaryFootnotes: Optional[str] = ...
+class HasBody(BaseModel):
+    body: str = Field(None, description="Body text (see Note on text contents)")
 
 
-class ContentItemWithFootnotes(ContentItemAllFields):
-    footnotes: Optional[str] = ...
-    documentaryFootnotes: Optional[str] = ...
+class HasFootnotes(BaseModel):
+    footnotes: str = Field(None, description="Official footnotes (see Note on text contents)")
+    documentaryFootnotes: str = Field(None, description="Editorial footnotes")
+
+
+class TextContent(HasFootnotes, HasBody):
+    pass
+
+
+class ContentItemWithBodyAndFootnotes(TextContent, ContentItemAllFields):
+    pass
+
+
+class ContentItemWithFootnotes(HasFootnotes, ContentItemAllFields):
+    pass
 
 
 class LawBasicFields(BaseModel):
+    """
+    **Law** (basic fields)
+    """
+
     type: str = "law"
-    id: str
-    url: str
-    firstPublished: str
-    sourceTimestamp: str
-    titleShort: Optional[str]
-    titleLong: str
-    abbreviation: str
-    slug: str
+    id: str = Field(..., description="Unique ID")
+    url: str = Field(..., description="Resource link")
+    firstPublished: str = Field(..., description="Initial publication date")
+    sourceTimestamp: str = Field(..., description="Modification time on gesetze-im-internet.de")
+    titleShort: str = Field(None, description="Title of the law (shortened)")
+    titleLong: str = Field(..., description="Title of the law")
+    abbreviation: str = Field(..., description="Abbreviated title")
+    slug: str = Field(..., description="URL-safe lowercased abbreviation")
 
     @classmethod
     def _attrs_dict_from_law(cls, law):
@@ -118,7 +133,7 @@ class LawBasicFields(BaseModel):
 
 
 class ContentItemBasicFieldsWithLaw(ContentItemBasicFields):
-    law: LawBasicFields
+    law: LawBasicFields = Field(..., description="Law (basic fields)")
 
     @staticmethod
     def model_class_from_item_type(item_type):
@@ -137,62 +152,91 @@ class ContentItemBasicFieldsWithLaw(ContentItemBasicFields):
 
 
 class HeadingBasicFields(ContentItemBasicFields):
+    """
+    **Heading** (basic fields)
+    """
     type: str = "heading"
 
 
 class HeadingBasicFieldsWithLaw(ContentItemBasicFieldsWithLaw):
+    """
+    **Heading** (basic fields and law)
+    """
     type: str = "heading"
 
 
 class HeadingAllFields(ContentItemWithFootnotes):
+    """
+    **Heading** (all fields)
+    """
     type: str = "heading"
 
 
 class ArticleBasicFields(ContentItemBasicFields):
+    """
+    **Article** (basic fields)
+    """
     type: str = "article"
 
 
 class ArticleBasicFieldsWithLaw(ContentItemBasicFieldsWithLaw):
+    """
+    **Article** (basic fields and law)
+    """
     type: str = "article"
 
 
 class ArticleAllFields(ContentItemWithBodyAndFootnotes):
+    """
+    **Article** (all fields)
+    """
     type: str = "article"
 
 
 class HeadingArticleBasicFields(ContentItemBasicFields):
+    """
+    **HeadingArticle** (basic fields)
+    """
     type: str = "headingArticle"
 
 
 class HeadingArticleBasicFieldsWithLaw(ContentItemBasicFieldsWithLaw):
+    """
+    **HeadingArticle** (basic fields and law)
+    """
     type: str = "headingArticle"
 
 
 class HeadingArticleAllFields(ContentItemWithBodyAndFootnotes):
+    """
+    **HeadingArticle** (all fields)
+    """
     type: str = "headingArticle"
 
 
+class PublicationInfoItem(BaseModel):
+    reference: str = Field(..., description="e.g. year, issue no., page")
+    periodical: str = Field(..., description="Name of publication")
+
+
+class StatusInfoItem(BaseModel):
+    comment: str = Field(..., description="Free-text status information")
+    category: str = Field(..., description='Any of "Aufh", "Stand", "Hinweis", "Neuf", "Sonst"')
+
+
 class LawAllFields(LawBasicFields):
-    extraAbbreviations: List[str]
-    publicationInfo: List[create_model(
-        'PublicationInfoItem',  # noqa
-        reference=(str, ...),
-        periodical=(str, ...)
-    )]
-    statusInfo: List[create_model(
-        'StatusInfoItem',  # noqa
-        comment=(str, ...),
-        category=(str, ...)
-    )]
-    notes: create_model(
-        'TextContent',  # noqa
-        body=(Optional[str], ...),
-        footnotes=(Optional[str], ...),
-        documentaryFootnotes=(Optional[str], ...)
-    )
-    attachments: dict
+    """
+    **Law** (all fields)
+    """
+    extraAbbreviations: List[str] = Field(..., description="Additional abbreviations")
+    publicationInfo: List[PublicationInfoItem] = Field(..., description="Publication information")
+    statusInfo: List[StatusInfoItem] = Field(..., description="Status information")
+    notes: TextContent
+    attachments: dict = Field(..., description="See Note on attachments")
     # Order in the Union matters: FastAPI tries one after the other and only skips types if there's a validation error.
-    contents: Optional[List[Union[ArticleAllFields, HeadingAllFields, HeadingArticleAllFields]]]
+    contents: List[Union[
+        ArticleAllFields, HeadingAllFields, HeadingArticleAllFields
+    ]] = Field(None, description="Contents of the law (articles, section headings)")
 
     @classmethod
     def _attrs_dict_from_law(cls, law, include_contents=False):
@@ -224,7 +268,7 @@ class LawAllFields(LawBasicFields):
 
 
 class LawResponse(BaseModel):
-    data: LawAllFields
+    data: LawAllFields = Field(..., description="The requested data")
 
     @classmethod
     def from_orm_model(cls, law):
@@ -232,25 +276,31 @@ class LawResponse(BaseModel):
 
 
 class PaginationLinks(BaseModel):
-    prev: Optional[str]
-    next: Optional[str]
+    """
+    Pagination links
+    """
+    prev: str = Field(None, description="Link to previous result page")
+    next: str = Field(None, description="Link to following result page")
 
 
 class Pagination(BaseModel):
-    total: int
-    page: int
-    per_page: int
+    """
+    Pagination information
+    """
+    total: int = Field(..., description="Total number of items")
+    page: int = Field(..., description="Result page number")
+    per_page: int = Field(..., description="Number of items per page")
 
 
 class LawsResponse(BaseModel):
-    data: list
+    data: List[Union[LawAllFields, LawBasicFields]] = Field(..., description="The requested data")
     links: PaginationLinks
     pagination: Pagination
 
 
 class ContentItemResponse(BaseModel):
     # Order in the Union matters: FastAPI tries one after the other and only skips types if there's a validation error.
-    data: Union[LawAllFields, ArticleAllFields, HeadingArticleAllFields]
+    data: Union[ArticleAllFields, HeadingArticleAllFields, HeadingAllFields] = Field(..., description="The requested data")
 
 
 class SearchResultsResponse(BaseModel):
@@ -259,6 +309,6 @@ class SearchResultsResponse(BaseModel):
         LawBasicFields,
         ArticleBasicFieldsWithLaw, ArticleBasicFields,
         HeadingArticleBasicFieldsWithLaw, HeadingArticleBasicFields
-    ]]
+    ]] = Field(..., description="The requested data")
     links: PaginationLinks
     pagination: Pagination
